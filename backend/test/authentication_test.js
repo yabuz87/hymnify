16c3cc0352f4e79871f66aa7385accbe6c08fe39
Yabets solomon <jabezsolomonz@gmail.com>
2025-10-26 20:11:52 +0300

    add: new testing file to store some testing cases to use readily in api testing tools like postman.

---
+++ backend/test/authentication_test.js
@@ -0,0 +1,16 @@
+// a testing peridicates for authentication functionalities.
+// These tests cover user signup, login, token generation, and token validation.
+// this json file is used to test in postman or other api testing tools.
+const signupTestData = {
+  "churchName": "St. Mary Church",
+  "choirName": "Heavenly Voices",
+  "location": {
+    "region": "Addis Ababa",
+    "city": "Addis Ketema",
+    "kebele": "12"
+  },
+  "email": "heavenlyvoices@gmail.com",
+  "password": "strongPass123",
+  "accessingPassword": "access123",
+  "profileImage": "https://res.cloudinary.com/example/image/upload/v1234/profile.jpg"
+}
