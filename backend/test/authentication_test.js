1adf7e733eecda4ed18fbf2c989559b427b8a519
Yabets solomon <jabezsolomonz@gmail.com>
2025-10-27 15:55:26 +0300

    add: test files are added to test populating the data into db.

--- backend/test/authentication_test.js
+++ backend/test/authentication_test.js
@@ -2,7 +2,7 @@
 // These tests cover user signup, login, token generation, and token validation.
 // this json file is used to test in postman or other api testing tools.
 const signupTestData = {
-  "churchName": "St. Mary Church",
+  "churchName": "Emanuel Church",
   "choirName": "Heavenly Voices",
   "location": {
     "region": "Addis Ababa",
@@ -13,4 +13,22 @@ const signupTestData = {
   "password": "strongPass123",
   "accessingPassword": "access123",
   "profileImage": "https://res.cloudinary.com/example/image/upload/v1234/profile.jpg"
+}
+
+const loginTestData = {
+
+      adminlogin:{
+"email":"heavenlyvoices@gmail.com",
+"password":"strongPass123"},
+
+clientLogin:{
+  "churchName": "Emanuel Church",
+  "choirName": "Heavenly Voices",
+  "location": {
+    "region": "Addis Ababa",
+    "city": "Addis Ketema",
+    "kebele": "12"
+  },
+  "accessingPassword": "access123"
+}
 }
