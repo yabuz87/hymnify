d58b1e0c0461bbaec780bb6bab0290c6045118ef
Yabets solomon <jabezsolomonz@gmail.com>
2026-02-06 15:49:13 +0300

    update: routing update to handle the verification steps in otp signing up route

--- backend/router/owner.router.js
+++ backend/router/owner.router.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import Owner from '../model/owner.model.js'
-import {loginAdmin,loginClient,logoutAdmin,signUp,logoutClient} from "../controller/ownership.controller.js"
+import {loginAdmin,loginClient,logoutAdmin,signUp,logoutClient,verifyEmail} from "../controller/ownership.controller.js"
 export const ownerRouter=express.Router()
 
 ownerRouter.post("/signup",signUp);
 ownerRouter.post("/login",loginAdmin);
+ownerRouter.post("/verify",verifyEmail);
 ownerRouter.post("/logout",logoutClient);
 ownerRouter.post("/client/login",loginClient);
 ownerRouter.post("/client/logout",logoutAdmin);
-
-
-
