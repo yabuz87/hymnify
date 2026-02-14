527aa2356e9102fbff98c89faf70f17c3021f521
Yabets solomon <jabezsolomonz@gmail.com>
2026-02-14 11:23:43 +0300

    update generate token function

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 dotenv.config();
 export const generateToken=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
+    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"2d"})
 res.cookie("jwt",token,{
-    maxAge:7*24*60*60*1000,
+    maxAge:2*24*60*60*1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !=="development"
+    secure: false,        // true only in HTTPS production
+    sameSite: "lax", 
 
 })
 return token;
