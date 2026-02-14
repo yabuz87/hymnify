71ead0156292bbcc6472d994c9d1a5c58818b60d
Yabets solomon <jabezsolomonz@gmail.com>
2026-02-14 11:24:22 +0300

    update: routing updated

--- backend/router/song.router.js
+++ backend/router/song.router.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import {protectRoute } from "../middleware/auth.middleware.js"
-import {uploadSong,getallpublic,deletesong,editsong} from '../controller/song.controller.js'
+import {uploadSong,getallpublic,deleteSong,editsong} from '../controller/song.controller.js'
 export const songRouter=express.Router()
 
 
 
 songRouter.get('/song/all',getallpublic)
-songRouter.post('/song/upload/:ownerId', uploadSong)
-songRouter.post('/song/delete',protectRoute,deletesong)
-songRouter.post('/song/edit',protectRoute,editsong)
+songRouter.post('/song/upload/:ownerId',protectRoute,uploadSong)
+songRouter.post('/song/delete/:ownerId',protectRoute,deleteSong)
+songRouter.post('/song/edit/:ownerId',protectRoute,editsong)
