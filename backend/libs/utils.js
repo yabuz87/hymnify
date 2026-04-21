ea00265200bbf3c083eb2ea46e693c59580caa06
Yabets solomon <jabezsolomonz@gmail.com>
2026-04-21 18:03:06 +0300

    feat: initialize backend project with JWT authentication utility and core dependencies

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -3,12 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const generateToken=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"2d"})
-res.cookie("jwt",token,{
-    maxAge:2*24*60*60*1000,
-    httpOnly:true,
-    secure: false,        // true only in HTTPS production
-    sameSite: "lax", 
-
-})
-return token;
+res.cookie("jwt", token, {
+        maxAge: 2 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // true only in HTTPS production
+        sameSite: "strict",
+    });
+    return token;
 }
