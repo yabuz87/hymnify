28de03a937b764659758c93bbe450894f7a47389
Yabets solomon <jabezsolomonz@gmail.com>
2026-04-22 16:42:04 +0300

    feat: implement JWT token generation and secure cookie setting utility

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const generateToken=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"2d"})
-res.cookie("jwt", token, {
+    res.cookie("jwt", token, {
         maxAge: 2 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // true only in HTTPS production
-        sameSite: "strict",
+        secure: true, // Required for sameSite: 'none'
+        sameSite: "none", // Required for cross-site cookies (Vercel to Render)
     });
     return token;
 }
